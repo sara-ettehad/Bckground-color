b1e1c4a33e5a520d5c116e50ca1ddbba7105dfac
Parisa Ettehad <92958591+parisa-ettd@users.noreply.github.com>
2022-12-28 22:45:48 -0500

    message

--- js/main.js
+++ js/main.js
@@ -15,5 +15,16 @@ colors.forEach(color =>{
     const button = document.createElement("button");
     button.style.backgroundColor = color;
 
+    button.onclick = function (){
+        document.body.style.backgroundColor = color;
+        document.querySelector("#current-color").innerHTML = color;
+    }
+
     document.querySelector("div.colors").appendChild(button);
-})
+});
+
+document.body.onload = function (){
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    
+    console.log(randomIndex);
+}
